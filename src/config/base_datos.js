b8f62e124a1e0a396447cd0f7c8d6ff09f2f0c72
Isis Cedeño <isis01sofic@gmail.com>
2025-11-26 17:45:10 -0400

    intento de conexion

--- src/config/base_datos.js
+++ src/config/base_datos.js
@@ -1,1 +1,19 @@
-const(pool)
+const{pool, DatabaseError} = require ('pg');
+require ('dotenv').config() ;
+
+const pool = new pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
+
+const probar = async () => {
+    try {
+    
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error.message );
+    }
+    
+}
