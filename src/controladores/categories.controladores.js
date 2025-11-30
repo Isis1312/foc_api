06ca1dcd178c4b4163914d7b33f8aeab12b35f49
josepernalete2 <jose00pg2@gmail.com>
2025-11-29 20:12:30 -0400

    categoria listo

---
+++ src/controladores/categories.controladores.js
@@ -0,0 +1,57 @@
+import { CategoriesServicio } from "../servicios/categories.servicios.js";
+
+export class CategoriesController {
+  constructor() { }
+
+  getAll = async (req, res) => {
+    const { message, status, data } = await CategoriesServices.getAll();
+    return res.status(status).json({
+      message,
+      data
+    });
+  };
+
+  getOne = async (req, res) => {
+    const { id } = req.params;
+
+    const { message, status, data } = await CategoriesServices.getById(Number(id));
+
+    return res.status(status).json({
+      message,
+      data
+    });
+  };
+
+  created = async (req, res) => {
+    const categoryData = req.body;
+
+    const { message, status, data } = await CategoriesServices.create(categoryData);
+
+    return res.status(status).json({
+      message,
+      data
+    });
+  };
+
+  updated = async (req, res) => {
+    const { id } = req.params;
+    const categoryData = req.body;
+
+    const { message, status, data } = await CategoriesServices.update(Number(id), categoryData);
+
+    return res.status(status).json({
+      message,
+      data
+    });
+  };
+
+  deleted = async (req, res) => {
+    const { id } = req.params;
+
+    const { message, status } = await CategoriesServices.delete(Number(id));
+
+    return res.status(status).json({
+      message
+    });
+  };
+}
---
+++ src/rutas/categories.rutas.js
@@ -0,0 +1,18 @@
+
+import { Router } from 'express';
+import { CatwgoriesController } from '../controladores/categories.controladores.js';
+
+
+const router = Router();
+const categories_controller= new CategoriesController()
+//obtener una lista
+router.get('/', categories_controller.getAll)
+//obtener uno
+router.get('/:id',categories_controller.getOne)
+//post
+router.post('/', categories_controller.created)
+//put
+router.put('/:id',categories_controller.updated)
+//delete
+router.delete('/:id',categories_controller.deleted)
+export default router;
